bef6a293fddd2c93a1fb0651e97a2b00e7693199
Nils P <arzisus@gmail.com>
2017-08-21 17:33:18 +0200

    Camera follows Player and ColorChanger
    Added Camerafollow script and a ColorChanger with its individual tag

---
+++ Color Switch/Assets/Scripts/player.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+    public class player : MonoBehaviour {
+
+	//setting variables
+	public float jumpforce = 10f;
+	public Rigidbody2D rb;
+	public SpriteRenderer srenderer;
+	public string currentColor;
+	public Color colorCyan;
+	public Color colorYellow;
+	public Color colorPurple;
+	public Color colorPink;
+
+	// Gets executed on first loadup
+	void Start ()
+	{
+		RandomColor ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0)) {
+
+			rb.velocity = Vector2.up * jumpforce;
+		}
+	}
+
+	// Collision Detection
+	void OnTriggerEnter2D (Collider2D col) {
+	
+        if (col.tag == ColorChanger)
+        {
+            RandomColor();
+        }
+		if (col.tag != currentColor) {
+		
+			Debug.Log ("you suck");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+			
+	}
+
+	// Set a random color
+	void RandomColor() {
+	
+		int index = Random.Range (0, 4);
+
+		switch (index) 
+		{
+
+		case 0:
+			currentColor = "Cyan";
+			srenderer.color = colorCyan;
+			break;
+		case 1:
+			currentColor = "Yellow";
+			srenderer.color = colorYellow;
+			break;
+		case 2:
+			currentColor = "Purple";
+			srenderer.color = colorPurple;
+			break;
+		case 3:
+			currentColor = "Pink";
+			srenderer.color = colorPink;
+			break;
+		}
+
+	}
+}
---
+++ Color Switch/Assets/Scripts/rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotator : MonoBehaviour {
+
+	public float rotatespeed = 100f;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0f, 0f, rotatespeed * Time.deltaTime);
+	}
+}
